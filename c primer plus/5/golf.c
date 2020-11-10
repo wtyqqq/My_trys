c2c74f6be8318c0acdebdb260c053382cdf3dbe9
幼稚的小樱桃 <623383371@qq.com>
2020-11-10 17:38:26 +0800

    Create golf.c

---
+++ c primer plus/5/golf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	int jane, tarzan, cheeta;
+	 
+	cheeta = tarzan = jane = 68;
+	printf("                 cheetra tarzan jane\n");
+	printf("First roun score %4d %8d %8d \n", cheeta, tarzan, jane);
+	return 0;
+}
