1539f3f8a7e65069fcf6893220aa6ac2dbe7695d
幼稚的小樱桃 <623383371@qq.com>
2020-11-10 23:58:26 +0800

    Create bottles.c

---
+++ c primer plus/5/bottles.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define MAX 100
+int main(void)
+{
+	int count = MAX + 1;
+	while(--count > 0){
+		printf("%d bottles of spring water on the wall,"
+			"%d bootles of spring water!\n", count, count);
+		printf("Take one down and pass");
+		printf(" %d bottles of spring water!\n\n", count - 1);
+	}
+	return 0;
+}
