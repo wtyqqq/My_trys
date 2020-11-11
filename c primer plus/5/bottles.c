68fffba90e29de34173ef791a58f674b8f90254d
幼稚的小樱桃 <623383371@qq.com>
2020-11-11 23:21:44 +0800

    add

--- c primer plus/5/bottles.c
+++ c primer plus/5/bottles.c
@@ -2,7 +2,7 @@
 #define MAX 100
 int main(void)
 {
-	int count = MAX + 1;
+	int count = MAX - 1;
 	while(--count > 0){
 		printf("%d bottles of spring water on the wall,"
 			"%d bootles of spring water!\n", count, count);
