8e78cf4273a54a3c8310aa7a4e6149c892740601
幼稚的小樱桃 <623383371@qq.com>
2020-11-13 22:12:20 +0800

    add

--- c primer plus/5/ex9.c
+++ c primer plus/5/ex9.c
@@ -1,28 +1,27 @@
 #include<stdio.h>
-const double ktc,ctk1,ctk2,ctk3;
-ktc = 273.16;
-ctk1 = 5;
-ctk2 = 9;
-ctk3 = 32;
-void Temperatures(double n);
-int main(void)
+const double ktc = 273.16;
+const double ctk1 = 5.0;
+const double ctk2 = 9.0;
+const double ctk3 = 32.0;
+int temperatures(double n);
+int main()
 {
 	int ret;
 	double ftem;
 	printf("Enter a F temperature:");
 	ret = scanf_s("%lf",&ftem);
-	while (ret = 1)
+	while (ret == 1)
 	{
-		Temperatures(ftem);
+		temperatures(ftem);
 		printf("Enter the next temperature:");
 		ret = scanf_s("%lf",&ftem);
 	}
 	return 0;
 }
-void Tmperatures(double n)
+int temperatures(double n)
 {
-	double ktem, ctem;
-		ctem = ctk1 / (ctk2 * (n - ctk3));
+		double ktem, ctem;
+		ctem = ctk1 / ctk2 * (n - ctk3);
 		ktem = ctem + ktc;
 		printf("The ctem is %.2lf, the ktem is %.2lf, the ftem is %.2f.\n",
 				ctem,ktem,n);
