39dadc12de3b52869ef62be78b4b2e785d4627de
幼稚的小樱桃 <623383371@qq.com>
2020-11-21 22:22:04 +0800

    add

---
+++ c primer plus/6/ex7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char letters[100];
+	int i;
+
+	printf("Please enter a word:\n");
+	while (scanf_s("%s", letters ) == 1)
+	{
+		for (i = strlen(letters) - 1; i >= 0; i--) 
+		{
+			printf("%c", letters[i]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
